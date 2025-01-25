015b3b5dbb504910180e642f1e36b5c9705d351f
Christian8D <christian.cosio1@gmail.com>
2025-01-25 21:00:50 +0100

    Clean-up

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { EventEmitter2 } from 'eventemitter2';
 import { v4 as uuid } from 'uuid';
 import { Employee } from './models/employee.model';
-// import { CreateEmployeeDto } from './dtos/create-employee.dto.ts';
+
 import { CreateEmployeeInput } from './dtos/create-employee.input';
 
 //decoupled version with event emitter
@@ -64,55 +64,3 @@ export class EmployeeService {
 
 }
 
-
-// coupled version without event emitter
-// }
-
-// export class EmployeeService {
-//   // In-memory storage for demonstration purposes
-//   private employees: Employee[] = [];
-
-//   createEmployee(dto: CreateEmployeeDto): Employee {
-//     const employee: Employee = {
-//       id: uuid(),
-//       ...dto,
-//     };
-//     this.employees.push(employee);
-//     return employee;
-//   }
-
-//   getEmployeeById(id: string): Employee {
-//     const employee = this.employees.find((emp) => emp.id === id);
-//     if (!employee) {
-//       throw new NotFoundException(`Employee with ID ${id} not found`);
-//     }
-//     return employee;
-//   }
-
-//   getAllEmployees(): Employee[] {
-//     return this.employees;
-//   }
-
-//   updateEmployee(
-//     id: string,
-//     updateFields: Partial<Omit<Employee, 'id' | 'name'>>,
-//   ): Employee {
-//     const employeeIndex = this.employees.findIndex((emp) => emp.id === id);
-//     if (employeeIndex < 0) {
-//       throw new NotFoundException(`Employee with ID ${id} not found`);
-//     }
-//     this.employees[employeeIndex] = {
-//       ...this.employees[employeeIndex],
-//       ...updateFields,
-//     };
-//     return this.employees[employeeIndex];
-//   }
-
-//   deleteEmployee(id: string): void {
-//     const employeeIndex = this.employees.findIndex((emp) => emp.id === id);
-//     if (employeeIndex < 0) {
-//       throw new NotFoundException(`Employee with ID ${id} not found`);
-//     }
-//     this.employees.splice(employeeIndex, 1);
-//   }
-// }
