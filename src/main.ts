e1081e909ffb1afbddf718a8514d58ebc9d3ec49
Christian8D <christian.cosio1@gmail.com>
2025-01-25 21:01:17 +0100

    clean-up

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,4 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   // Global validation pipe to automatically validate incoming DTOs
-//   app.useGlobalPipes(new ValidationPipe({
-//     whitelist: true,
-//     forbidNonWhitelisted: true,
-//     transform: true
-//   }));
-
-//   await app.listen(3000);
-//   console.log(`Application is running on: http://localhost:3000`);
-// }
-// bootstrap();
-
-
-// main.ts
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
